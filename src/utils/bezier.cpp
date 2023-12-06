51f0ae2b105f4de364e7160e6bf0872a92ff782a
jake maggie ronan <jake@Jakes-MacBook-Pro.local>
2023-12-06 10:16:58 -0500

    work on Bezier curves

--- src/utils/bezier.cpp
+++ src/utils/bezier.cpp
@@ -7,13 +7,13 @@ Bezier::Bezier()
 
 }
 
-Bezier::drawCurvePoint(std::vector(glm::vec4), t):
-    if(points.length==1):
-    draw(points[0])
-    else:
-           newpoints=array(points.size-1)
-           for(i=0; i<newpoints.length; i++):
-    x = (1-t) * points[i].x + t * points[i+1].x
-                                    y = (1-t) * points[i].y + t * points[i+1].y
-                                  newpoints[i] = new point(x,y)
-    drawCurvePoint(newpoints, t)
+//Bezier::drawCurvePoint(std::vector(glm::vec4), t):
+//    if(points.length==1):
+//    draw(points[0])
+//    else:
+//           newpoints=array(points.size-1)
+//           for(i=0; i<newpoints.length; i++):
+//    x = (1-t) * points[i].x + t * points[i+1].x
+//                                    y = (1-t) * points[i].y + t * points[i+1].y
+//                                  newpoints[i] = new point(x,y)
+//    drawCurvePoint(newpoints, t)
